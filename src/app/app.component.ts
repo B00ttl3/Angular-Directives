72a62781a11177538ed0e253e20416b47f5d11a4
B00ttl3 <mumair3825@gmail.com>
2024-09-14 00:40:05 +0500

    added some events and style files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,21 @@ export class AppComponent {
   title: string = 'This loaded dynamically';
   imgURL: string = 'https://i5.walmartimages.com/seo/HP-Stream-14-Laptop-Intel-Celeron-N4000-4GB-SDRAM-32GB-eMMC-Office-365-1-yr-Royal-Blue_4f941fe6-0cf3-42af-a06c-7532138492fc_2.cb8e85270e731cb1ef85d431e49f0bf2.jpeg';
   isDisabled: boolean = true;
-  isActive: boolean = false;
+  isActive: boolean = true;
   fruiteName: string = 'apple';
+
+  buttonClick() {
+    alert("button clicked");
+  }
+
+  keyEnter(event:any){
+    // console.log(event.keyCode)
+    if(event.keyCode == 13){
+      console.log('Ente Key Pressed');
+    }
+  }
+
+  keyupFiltering() {
+    console.log('KeyUp Filtering');
+  }
 }
