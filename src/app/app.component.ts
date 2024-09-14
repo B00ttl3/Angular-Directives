e7a9c92fb29cc6efb6f19dbb34a31fe7d87056c9
B00ttl3 <mumair3825@gmail.com>
2024-09-14 23:34:22 +0500

    added one way and two way data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,22 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AppNavbar } from './navbar/navbar.component';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, AppNavbar, HeaderComponent],
+  imports: [RouterOutlet, AppNavbar, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-<<<<<<< HEAD
   title: string = 'This loaded dynamically';
   imgURL: string = 'https://i5.walmartimages.com/seo/HP-Stream-14-Laptop-Intel-Celeron-N4000-4GB-SDRAM-32GB-eMMC-Office-365-1-yr-Royal-Blue_4f941fe6-0cf3-42af-a06c-7532138492fc_2.cb8e85270e731cb1ef85d431e49f0bf2.jpeg';
   isDisabled: boolean = true;
   isActive: boolean = true;
   fruiteName: string = 'apple';
+
+  userName: string = 'Umair Munir'
 
   buttonClick() {
     alert("button clicked");
@@ -28,10 +30,12 @@ export class AppComponent {
     }
   }
 
-  keyupFiltering() {
-    console.log('KeyUp Filtering');
+  keyupFiltering(user:HTMLInputElement) {
+    console.log(user.value);
   }
-=======
-  title = 'firstAngularApp';
->>>>>>> 362534fb4fa23f041ee0ba4761de21b310ea2ca4
+
+  updateUserName(username:HTMLInputElement){
+    this.userName = username.value;
+    console.log(this.userName);
+  }
 }
